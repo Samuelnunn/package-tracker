41abe700fc70f3aceea95a859d44f8b049414475
Samuel <samuelnunn90@gmail.com>
2021-02-11 15:56:26 -0500

    form done-ish

--- app/shipping_form.py
+++ app/shipping_form.py
@@ -1,13 +1,18 @@
 from wtforms.validators import DataRequired
 from flask_wtf import FlaskForm
-from wtforms import BooleanField, StringField, SelectField
+from wtforms import BooleanField, StringField, SelectField, SubmitField
 from wtforms.widgets.core import Select
 from map.map import map
+
+origin_values = [(k, k) for k in map.keys()]
+destination_values = [(k, k) for k in map.keys()]
+print(origin_values)
+
 
 class ShippingForm(FlaskForm):
     sender_name = StringField("Sender Name", [DataRequired()])
     recipient_name = StringField("Recipient Name", [DataRequired()])
-    origin = SelectField("Origin", [DataRequired()])
-    destination = SelectField("Destination", [DataRequired()])
-    express_shipping = BooleanField("Express Shipping",[DataRequired()])
-    
+    origin = SelectField("Origin", [DataRequired()], choices=origin_values)
+    destination = SelectField("Destination", [DataRequired()], choices=destination_values)
+    express_shipping = BooleanField("Express Shipping", [DataRequired()])
+    submit = SubmitField("Submit")

04840ea3cd1be5e8166d93f7405ffebae4e674a6
luke <lukebraun88@att.net>
2021-02-11 15:48:51 -0600

    database acceptance

--- package_tracker.py
+++ package_tracker.py
@@ -1,10 +1,14 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect
 from app.config import Config
 from app.shipping_form import ShippingForm
+from app.models import db
+from flask_migrate import Migrate
+from app.models import Package, db
 
 app = Flask(__name__)
 app.config.from_object(Config)
-
+db.init_app(app)
+migrate = Migrate(app, db)
 
 @app.route("/")
 def root():
@@ -14,4 +18,14 @@ def root():
 @app.route("/new_package", methods=["GET", "POST"])
 def new_package():
     form = ShippingForm()
+    if form.validate_on_submit():
+        data = form.data
+        new_package = Package(sender=data["sender_name"],
+                              recipient=data["recipient_name"],
+                              origin=data["origin"],
+                              destination=data["destination"],
+                              location=data["origin"])
+        db.session.add(new_package)
+        db.session.commit()
+        return redirect('/')
     return render_template("shipping_request.html", form=form)
